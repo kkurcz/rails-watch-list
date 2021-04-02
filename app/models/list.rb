14c74808a2d6563d31b9796a22c187eef4a9cb5b
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-04-02 14:04:38 +0800

    setup cloudinary and active storage

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,7 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   # the moviews are stored in bookmarks so need to use through
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
 end
